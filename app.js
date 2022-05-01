be9ba76f50bae36ce5d37a7b3b8ddd7a541fab06
William Barnes <jakebarnes1989@gmail.com>
2022-05-01 12:21:21 -0400

    Updated message to include button, non-functional

--- app.js
+++ app.js
@@ -14,9 +14,30 @@ const app = new App ({
 //#region Message Listener
 
 app.message('hello', async ({ message, say }) => {
-    
+
     // Say sends a message to the channel where the event was triggered
-    await say(`Hey there <@${message.user}>!`);
+    await say({
+        
+        blocks: [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": `Hey there <@${message.user}>!`
+              },
+              "accessory": {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click Me"
+                },
+                "action_id": "button_click"
+              }
+            }
+          ],
+          text: `Hey there <@${message.user}>!`
+        });
+
 });
 
 //#endregion
